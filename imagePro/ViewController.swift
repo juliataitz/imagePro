816ee4e6fbc69d92bc1f02f76b14515c2e1985cd
juliataitz <juliataitz@gmail.com>
2014-12-02 19:54:04 -0500

    added connections

--- imagePro/ViewController.swift
+++ imagePro/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var PhotoOpeningView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ChooseImage(sender: AnyObject) {
+    }
 
+    @IBAction func ApplyFilter(sender: AnyObject) {
+    }
+    @IBAction func SaveImage(sender: AnyObject) {
+    }
 }
 
