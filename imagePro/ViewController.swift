b44fdaab8b1507109dfdfeb0e7da970f21a7453e
juliataitz <juliataitz@gmail.com>
2014-12-02 19:48:15 -0500

    first commit

---
+++ imagePro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imagePro
+//
+//  Created by Julia Taitz on 12/2/14.
+//  Copyright (c) 2014 Julia Taitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
